d51b095bdd728836fc96ab04d409de231a325887
Matthew Beebe <mattbb24@gmail.com>
2018-04-13 11:10:59 -0400

    Basic license plate checker

---
+++ LicensePlate.rb
@@ -0,0 +1,10 @@
+class LicensePlate
+  puts 'Please enter 7 characters and a space in any order.  This will check if it matches current(2018) requirements of
+non-custom Ohio License plates'
+  @plate = gets
+  if @plate =~ /[A-Z]{3} [0-9]{4}/
+    puts 'the plate matches the requirements'
+  else
+    puts 'The license plate requirements are not met'
+end
+end
